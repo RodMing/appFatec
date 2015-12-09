288eb5bdc7b62edab05e6cb901cc6c5153d5872b
Rodrigo Mingattos <rodrigo_mingattos@hotmail.com>
2015-12-08 20:25:03 -0500

    teste

--- app/Http/Controllers/Cliente.php
+++ app/Http/Controllers/Cliente.php
@@ -8,22 +8,33 @@ class Cliente extends Controller
 {
     public function index(Request $request)
     {
-        $jsonObject = $request->input('jsonObject_');
-
+        $jsonObject = json_decode(json_encode($request->all()));
+        \Log::info(
+            json_encode($jsonObject)
+        );
+        $jsonObject = json_decode($jsonObject->jsonObject_, true);
+        $jsonObject['user'] = array_values($jsonObject['user']);
+        
         if ($jsonObject['method'] == 'save-user') {
             $gcm = \App::make('App\GcmModel');
-            $gcm->registration_id = $jsonObject['user']['registrationId'];
+            $gcm->registration_id = $jsonObject['user'][1];
             try {
                 $result = $gcm->save();
+                $id = (string)$gcm->id;
             } catch (\Exception $e) {
                 $result = false;
+                $id = (string)'';
             }
+            \Log::info(
+                json_encode($result)
+            );
+            return response()->json(
+                [
+                    'id' => $id,
+                    'result' => "{'id':$id, 'registrationId': $gcm->registration_id}"
+                ]
+            );
         }
-
-        return response()->json(
-            [
-                'result' => $result
-            ]
-        );
+        return response()->json(false);
     }
 }
